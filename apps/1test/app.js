0ab1ecc82dc9a0eb6929798b0674c945eef6d15d
hashimtechcampus <48765583+hashimtechcampus@users.noreply.github.com>
2021-10-06 19:57:10 +0300

    Update app.js

--- apps/1test/app.js
+++ apps/1test/app.js
@@ -1,12 +1,40 @@
-// place your const, vars, functions or classes here
+var counter = 0;
 
-// special function to handle display switch on
-Bangle.on('lcdPower', (on) => {
-  if (on) {
-    // call your app function here
-    // If you clear the screen, do Bangle.drawWidgets();
-  }
-});
+function updateScreen() {
+  g.clearRect(0, 50, 250, 150);
+  g.setColor(0xFFFF);
+  g.setFont("Vector",40).setFontAlign(0,0);
+  g.drawString(Math.floor(counter), g.getWidth()/2, 100);
+  g.drawString('-', 45, 100);
+  g.drawString('+', 185, 100);
+}
 
-g.clear();
-// call your app function here
+
+
+setWatch(() => {
+  counter += 1;
+  if(counter==8){
+    counter=7;}
+  updateScreen();
+}, BTN1, {repeat:true});
+
+
+setWatch(() => {
+  counter -= 1;
+  if(counter==-1){
+    counter=0;}
+  updateScreen();
+}, BTN3, {repeat:true});
+
+
+setWatch(() => {
+  counter = 0;
+  updateScreen();
+}, BTN2, {repeat:true});
+
+g.clear(1).setFont("6x8");
+g.drawString(' counter for twaf', 25, 200);
+
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+updateScreen();
